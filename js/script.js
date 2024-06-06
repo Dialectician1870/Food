9748f65b50c8dc78317ac153eb285d20d48c0093
Shakal <pachan222@mail.ru>
2024-06-06 23:40:33 +0300

    48 lesson, юзаем классы!

--- js/script.js
+++ js/script.js
@@ -103,8 +103,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const btns = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
-        closeBtn = document.querySelector('[data-close]'),
-        modalTimerId = setTimeout(openModal, 1000);
+        closeBtn = document.querySelector('[data-close]');
+        // modalTimerId = setTimeout(openModal, 1000);
 
     btns.forEach(item => {
         item.addEventListener('click', openModal);
@@ -148,4 +148,80 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector)
+            this.transfer = 27;
+            this.changeToUAH();
+            // вернет новый this.price
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        `Меню "Фитнес"`,
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+        9,
+        '.menu .container'
+    ).render();
+    // вместо const div = new MenuItem();
+    // div.render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        `Меню “Премиум”`,
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+        15,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+       " img/tabs/post.jpg",
+        "post",
+        `Меню "Постное"`,
+        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
+        13,
+        '.menu .container'
+    ).render();
+
+    const menuItems = document.querySelectorAll('.menu__item');
+
+    menuItems.forEach((item, i) => {
+        if (i < 3) {
+            item.remove();
+        }
+    });
+    // это чтобы не удалять хард код, как челик
+    // типо жоский прогер
 })
